9ccbe0be37b6f89f9bb4681aaa7e7f34a2c4eb86
Mrognor <timurlego1@yandex.ru>
2024-06-03 15:34:05 +0300

    Finish basic sha2 algo

--- Sha2.cpp
+++ Sha2.cpp
@@ -22,9 +22,23 @@ const std::uint32_t K[64] = {
     0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
 };
 
-uint32_t RightRotate(const uint32_t& intToRotate, const uint32_t& rotateSize)
+/**
+    \brief A function for rotating a number
+    
+    The rotation of a number is equivalent to a cyclic shift of that number
+
+    \param [in] digitToRotate the number to be rotated
+    \param [in] rotateLen The number to rotate by
+
+    \example Lets rotate 1234 number by 3
+    1234 = 0b00000000000000000000010011010010
+    RightRotate(1234) = 0b01000000000000000000000010011010
+
+    \return new rotated digit
+*/
+uint32_t RightRotate(const uint32_t& digitToRotate, const uint32_t& rotateLen) noexcept
 {
-    return (intToRotate >> rotateSize) | (intToRotate << (sizeof(uint32_t) * 8 - rotateSize));
+    return (digitToRotate >> rotateLen) | (digitToRotate << (sizeof(uint32_t) * 8 - rotateLen));
 }
 
 /// \brief Convert uint32 to string with hex form of this number
@@ -59,13 +73,14 @@ std::string Uint32ToHexForm(std::uint32_t a) noexcept
 /**
     \brief The function considers the correct padding for the data
 
-    \param [in] data a pointer to the char array to find the padding for
+    \param [in] data a pointer to the char, starting for which you need to find the padding
     \param [in] arrayLen the number of elements in the data arr
     \param [in] dataLen the length of the source data
+    \param [out] destination a pointer to the char in which padding should be written
 
-    \return Returns a string with the correct padding of length 64 or 128, depending on the length of the source data
+    \return returns the length of the padding. 64 if the length of the source data was less than 56, otherwise it will return 128
 */
-int DataPadding_Sha2(const char* data, const std::size_t& dataLen, const std::size_t& sourceLen, char* destination)
+int DataPadding_Sha2(const char* data, const std::size_t& dataLen, const std::size_t& sourceLen, char* destination) noexcept
 {
     // Variable to store padding length
     int res;
@@ -105,24 +120,40 @@ int DataPadding_Sha2(const char* data, const std::size_t& dataLen, const std::si
     return res;
 }
 
-void Sha2Step(const char* data, const std::size_t& dataPos, std::uint32_t& h0, std::uint32_t& h1, std::uint32_t& h2, std::uint32_t& h3, std::uint32_t& h4, std::uint32_t& h5, std::uint32_t& h6, std::uint32_t& h7)
+/**
+    \brief Sha2 hashing step
+    
+    The function calculates the sha2 hash sum for a 64 byte block of data
+
+    \param [in] data a pointer to the array to calculate the hash for
+    \param [in] offset a shift to indicate the beginning of the data block for which the hash is to be calculated
+    \param [in, out] h0 internal state variable 0
+    \param [in, out] h1 internal state variable 1
+    \param [in, out] h2 internal state variable 2
+    \param [in, out] h3 internal state variable 3
+    \param [in, out] h4 internal state variable 4
+    \param [in, out] h5 internal state variable 5
+    \param [in, out] h6 internal state variable 6
+    \param [in, out] h7 internal state variable 7
+*/
+void Sha2Step(const char* data, const std::size_t& offset, std::uint32_t& h0, std::uint32_t& h1, std::uint32_t& h2, std::uint32_t& h3, std::uint32_t& h4, std::uint32_t& h5, std::uint32_t& h6, std::uint32_t& h7) noexcept
 {
     // Words array
     std::uint32_t words[64] = {0};
 
     // Join 4 chars from data into 16 uint32_t numbers and save it to words array
     for (int i = 0; i < 64; i += 4)
-        words[i >> 2] = (static_cast<std::uint32_t>(static_cast<unsigned char>(data[i + dataPos])) << 24) | 
-            (static_cast<std::uint32_t>(static_cast<unsigned char>(data[i + 1 + dataPos])) << 16) |
-            (static_cast<std::uint32_t>(static_cast<unsigned char>(data[i + 2 + dataPos])) << 8) | 
-            static_cast<std::uint32_t>(static_cast<unsigned char>(data[i + 3 + dataPos]));
+        words[i >> 2] = (static_cast<std::uint32_t>(static_cast<unsigned char>(data[i + offset])) << 24) | 
+            (static_cast<std::uint32_t>(static_cast<unsigned char>(data[i + 1 + offset])) << 16) |
+            (static_cast<std::uint32_t>(static_cast<unsigned char>(data[i + 2 + offset])) << 8) | 
+            static_cast<std::uint32_t>(static_cast<unsigned char>(data[i + 3 + offset]));
 
     // Fill last 48 uint32_t numbers
     for (int i = 16; i < 64; ++i)
         words[i] = words[i - 16] + (RightRotate(words[i - 15], 7) ^ RightRotate(words[i - 15], 18) ^ (words[i - 15] >> 3)) +
             words[i - 7] + (RightRotate(words[i - 2], 17) ^ RightRotate(words[i - 2], 19) ^ (words[i - 2] >> 10));
 
-    // Temple variables
+    // Temporary variables
     std::uint32_t a = h0, b = h1, c = h2, d = h3, e = h4, f = h5, g = h6, h = h7;
 
     // 64 rounds to calculate hash for data block
@@ -134,36 +165,57 @@ void Sha2Step(const char* data, const std::size_t& dataPos, std::uint32_t& h0, s
         h = g; g = f; f = e; e = d + temp1; d = c; c = b; b = a; a = temp1 + temp2;
     }
 
+    // Add temporary variables to hash
     h0 += a, h1 += b, h2 += c, h3 += d, h4 += e, h5 += f, h6 += g, h7 += h;
 }
 
-std::string HashSha2(const char* data, const std::size_t& dataLen)
+/**
+    \brief A function for calculating the hash sum using the sha2 algorithm
+
+    \param [in] data a pointer to the array to calculate the hash for
+    \param [in] dataLen data array length
+
+    \return a string with a sha2 hash sum
+*/
+std::string HashSha2(const char* data, const std::size_t& dataLen) noexcept
 {
     // Begin hash values
     std::uint32_t h0 = 0x6a09e667, h1 = 0xbb67ae85, h2 = 0x3c6ef372, h3 = 0xa54ff53a, h4 = 0x510e527f, h5 = 0x9b05688c, h6 = 0x1f83d9ab, h7 = 0x5be0cd19;
 
+    // Handle 64 byte chunks
     for (std::size_t i = 0; i < dataLen >> 6; ++i)
         Sha2Step(data, i << 6, h0, h1, h2, h3, h4, h5, h6, h7);
 
-    // Padding source string
+    // Padding source data
     char padding[128];
     int paddingLen = DataPadding_Sha2(data + (dataLen & ~0b00111111), dataLen & 0b00111111, dataLen, padding);
 
+    // Calculate hash for padded data
     Sha2Step(padding, 0, h0, h1, h2, h3, h4, h5, h6, h7);
 
+    // If padding length is 128 then calculate hash for last block
     if (paddingLen == 128)
         Sha2Step(padding, 64, h0, h1, h2, h3, h4, h5, h6, h7);
 
+    // Return calculated hash
     return Uint32ToHexForm(h0) + Uint32ToHexForm(h1) + Uint32ToHexForm(h2) + Uint32ToHexForm(h3) + Uint32ToHexForm(h4) + Uint32ToHexForm(h5) + Uint32ToHexForm(h6) + Uint32ToHexForm(h7);
 }
 
-std::string HashSha2(const std::string& str)
+/**
+    \brief A function for calculating the hash sum using the sha2 algorithm
+
+    \param [in] str the string to calculate the hash for
+
+    \return a string with a sha2 hash sum
+*/
+std::string HashSha2(const std::string& str) noexcept
 {
     return HashSha2(str.c_str(), str.length());
 }
 
 std::string Sha2File(const std::string& fileName) noexcept
 {
+    // Begin hash values
     std::uint32_t h0 = 0x6a09e667, h1 = 0xbb67ae85, h2 = 0x3c6ef372, h3 = 0xa54ff53a, h4 = 0x510e527f, h5 = 0x9b05688c, h6 = 0x1f83d9ab, h7 = 0x5be0cd19;
 
     // Open file
@@ -189,6 +241,7 @@ std::string Sha2File(const std::string& fileName) noexcept
             // Read chunk from input file
             file.read(fileDataChunk, CHUNK_SIZE);
 
+            // Calculate hash steps
             for (std::size_t i = 0; i < CHUNK_SIZE; i += 64)
                 Sha2Step(fileDataChunk, i, h0, h1, h2, h3, h4, h5, h6, h7);
         }
@@ -209,12 +262,14 @@ std::string Sha2File(const std::string& fileName) noexcept
     char padding[128];
     int paddingLen = DataPadding_Sha2(fileDataChunk + (counter & ~0b00111111), counter & 0b00111111, fileSize, padding);
 
+    // Calculate hash for padded data
     Sha2Step(padding, 0, h0, h1, h2, h3, h4, h5, h6, h7);
 
+    // If padding length is 128 then calculate hash for last block
     if (paddingLen == 128)
         Sha2Step(padding, 64, h0, h1, h2, h3, h4, h5, h6, h7);
 
-    // Return changed initial uints converted to string with hex form
+    // Return calculated hash
     return Uint32ToHexForm(h0) + Uint32ToHexForm(h1) + Uint32ToHexForm(h2) + Uint32ToHexForm(h3) + Uint32ToHexForm(h4) + Uint32ToHexForm(h5) + Uint32ToHexForm(h6) + Uint32ToHexForm(h7);
 }
 
@@ -224,6 +279,3 @@ int main()
 
     std::cout << Sha2File("Sha2.cpp") << std::endl;
 }
-
-// b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9
-// b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9
